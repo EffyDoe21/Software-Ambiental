cd594f11e2e7f5f9c1005fdb66fc8aa2aedf1165
EffyDoe21 <es.becerra@ciaf.edu.co>
2024-05-28 02:46:20 -0500

    Actualización Perfil

---
+++ Base/Perfil/Perfil de Usuario.js
@@ -0,0 +1,16 @@
+// Función para cambiar el avatar del usuario
+const defaultFile = 'https://stonegatesl.com/wp-content/uploads/2021/01/avatar-300x300.jpg';
+
+const file = document.getElementById( 'foto' );
+const img = document.getElementById( 'img' );
+file.addEventListener( 'change', e => {
+  if( e.target.files[0] ){
+    const reader = new FileReader( );
+    reader.onload = function( e ){
+      img.src = e.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }else{
+    img.src = defaultFile;
+  }
+} ); 
